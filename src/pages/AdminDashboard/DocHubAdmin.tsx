8896399d26eed0333e963ffca0ee222e535293e4
jimmyvallejo <jrv1295@gmail.com>
2025-03-21 16:33:58 -0400

    Admin dochub

---
+++ src/pages/AdminDashboard/DocHubAdmin.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import AdminLayout from "components/common/layouts/AdminLayout";
+import { userSidebarOption } from "consts/dashboard.const";
+import { HeaderTitle } from "interfaces/dashboard/headerTitle.enum";
+import { getUser } from "api/dashboard.api";
+import { ViewProps } from "interfaces/dashboard/viewProps.interface";
+import DocHub from "components/userDashboard/DocHub";
+
+
+const AdminDocHubView: React.FC<ViewProps> = ({isSideBarCollapsed, setIsSideBarCollapsed}) => {
+    const { data, isLoading } = getUser();
+
+    return (
+        <AdminLayout 
+            isUser={HeaderTitle.Client}
+            isDarkBackground
+            title={"Doc Hub"}
+            user={data as any}
+            options={userSidebarOption}
+            isSideBarCollapsed={isSideBarCollapsed}
+            setIsSideBarCollapsed={setIsSideBarCollapsed}
+        >
+            <DocHub/>
+        </AdminLayout>
+    );
+};
+
+export { AdminDocHubView };
